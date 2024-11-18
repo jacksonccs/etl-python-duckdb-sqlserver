c7a985ac8a41403f9d6e0239851fb7d69ae2f7e4
jacksonccs <jackson.santiago@grupoboticario.com.br>
2024-11-18 17:51:31 -0300

    update docker

--- main_data.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-import pandas as pd
-
-conn = duckdb.connect('c:/GAVB/Neogrid/duckDB/learning_sql.db')
-
-conn.sql('select Location from location_data limit 3')
-conn.sql('show databases')
-
-print(conn.sql('from location_data limit 3'))
